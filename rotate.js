0988e7784cc5e8a824add7794eb37f78f2f569d2
sammykirigha <dkirigha18@gmail.com>
2021-07-04 22:18:55 +0300

    added a transpose algorithm

--- rotate.js
+++ rotate.js
@@ -7,14 +7,12 @@ function rotateMatrix(mat) {
     let n = mat.length;
     for (let i = 0; i < Math.floor(n / 2); i++){
         for (let j = i; j < n - i - 1; j++){
-            let temp = mat[i][j];
+            let tem = mat[i][j];
             mat[i][j] = mat[n - 1 - j][i]
             mat[n - 1 - j][i] = mat[n - i -1][n - j - 1]
             mat[n - i - 1][n - j - 1] = mat[j][n - i - 1]
-            mat[j][n - i - 1] = temp;
+            mat[j][n - i - 1] = tem;
         }
-        console.log(n)
-        console.log(temp)
     }
     return mat;
 }
@@ -25,3 +23,32 @@ console.log(rotateMatrix([
     [9, 10, 11, 12],
     [13, 14, 15, 16]
 ]));
+
+//METHOD 2 TRANSPOSE AND THEN REVERSE EACH ROW
+
+let rotateTranspose = (matrix) => {
+    let n = matrix.length;
+    for(let i = 0; i < n; i++){
+        for (let j = i; j < n; j++){
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    
+    for(let i = 0; i < n; i++){
+        let start = 0;
+        let end = n - 1;
+        while (start < end) {
+            let temp = matrix[i][start];
+            matrix[i][start] = matrix[i][end];
+            matrix[i][end] = temp;
+            start++;
+            end--;
+        }   
+    }
+}
+
+console.log(rotateTranspose([[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16]]))
+
+
